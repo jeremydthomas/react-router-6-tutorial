7b4b0eef3eb6e357479dcc395bcc1368a1c7e5df
jeremydthomas <tommyhilfiger12@hotmail.com>
2022-08-27 21:45:17 -0500

    single products

---
+++ src/components/StyledNavbar.js
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+	return (
+		<nav className="navbar">
+			<NavLink
+				to="/"
+				className={({ isActive }) => (isActive ? "link active" : "link")}
+			>
+				Home
+			</NavLink>
+			<NavLink
+				className={({ isActive }) => (isActive ? "link active" : "link")}
+				to="/about"
+			>
+				About
+			</NavLink>
+			<NavLink
+				className={({ isActive }) => (isActive ? "link active" : "link")}
+				to="/products"
+			>
+				Products
+			</NavLink>
+		</nav>
+	);
+};
+
+export default Navbar;
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,9 +1,13 @@
+import { Link, useParams } from "react-router-dom";
 const SingleProduct = () => {
-  return (
-    <section className='section product'>
-      <h2>single product</h2>
-    </section>
-  );
+	const { productId } = useParams();
+
+	return (
+		<section className="section product">
+			<h4>{productId}</h4>
+			<Link to="/products">Back to products</Link>
+		</section>
+	);
 };
 
 export default SingleProduct;
